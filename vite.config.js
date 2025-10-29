b73f9c551da4cd8b78b3e29a6fa52b40366006a6
Shaik sohail <shaik.sohail0728@gmail.com>
2025-10-29 11:54:57 +0530

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sohail-portfolio/', // 👈 Must match your repo name exactly
+  base: '/sohail-portfolio/',
   build: {
-    outDir: 'dist',
-  },
+    outDir: 'dist'
+  }
 });
