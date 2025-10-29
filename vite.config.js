fecde496bc6b0c06bdec7b9a1b58f57cdb9b2365
Shaik sohail <shaik.sohail0728@gmail.com>
2025-10-29 10:59:26 +0530

    Update Vite config for base path and formatting

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  base: '/sohail-portfolio/', // 👈 Must match your repo name exactly
   build: {
     outDir: 'dist',
   },
-})
+});
